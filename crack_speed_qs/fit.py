04a64f5bd1bbe4fd0a06947b1f07242a436797cb
Frederic Marazzato <marazzato@lsu.edu>
2020-12-02 14:21:35 -0600

    Added a file for the fit of the crack speed.

---
+++ crack_speed_qs/fit.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as st
+import sys
+
+size_ref = 20
+
+#values = np.loadtxt('dt_0_0_1/length_crack_%i.txt' % size_ref)
+values = np.loadtxt('dt_1_E_3/length_crack_%i.txt' % size_ref)
+
+mu = 0.2
+H = 1
+Gc = 0.01
+ref_vel = np.sqrt(mu*H/Gc) #ref velocity (computed by me !)
+print(ref_vel)
+
+x = values[:,0]
+y = values[:,1]
+
+
+slope, intercept, r_value, p_value, std_err = st.linregress(x, y)
+print(slope)
+print('Relative error slope: %.3e%%' % (np.absolute(ref_vel - slope) / ref_vel * 100))
+print(r_value)

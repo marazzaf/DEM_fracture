874fa7d0e20e21b3bcb077dfad3923d9a4822786
marazzaf <frederic.marazzato@ponts.org>
2021-06-29 10:48:44 -0500

    Looks like the consistent computation of reactions forces is better. Check if diminishes with crack advance.

--- mode_2/mode_2.py
+++ mode_2/mode_2.py
@@ -30,12 +30,12 @@ mesh = Mesh()
 #size_ref = 2
 #with XDMFFile("mesh/fine.xdmf") as infile:
 #    infile.read(mesh)
-#size_ref = 1
-#with XDMFFile("mesh/coarse.xdmf") as infile:
-#    infile.read(mesh)
-size_ref = 3
-with XDMFFile("mesh/very_fine.xdmf") as infile:
+size_ref = 1
+with XDMFFile("mesh/coarse.xdmf") as infile:
     infile.read(mesh)
+#size_ref = 3
+#with XDMFFile("mesh/very_fine.xdmf") as infile:
+#    infile.read(mesh)
 h = mesh.hmax()
 print(h)
 #finir plus tard pour taille des mailles.
@@ -210,8 +210,8 @@ while u_D.t < T:
             
 
             #jeremy
-            v = interpolate(Constant((1,0)), U_CR)
-            load_test = np.dot(A*v.get_local() - L, v.get_local())
+            v = problem.trace_matrix.T * interpolate(Constant((1,0)), U_CR).vector().get_local()
+            load_test = np.dot(A*u, v)
             ld.write('%.5e %.5e %.5e\n' % (u_D.t, assemble(load), load_test))
 
         cracking_facets = set()

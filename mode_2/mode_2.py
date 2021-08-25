Corrected the mistake in the computation of consistent reaction forces. Question is now: why are they much smaller than expected...

mode_2/mode_2.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from dolfin import *
-from scipy.sparse.linalg import spsolve
 from DEM_cracking.DEM import *
 from DEM_cracking.miscellaneous import *
 from DEM_cracking.cracking import *
@@ -10,19 +9,19 @@ parameters["form_compiler"]["cpp_optimize"] = True
 parameters["form_compiler"]["optimize"] = True
 
 # elastic parameters
-#mu = 80.77e9 #Ambati et al
-#lambda_ = 121.15e9 #Ambati et al
-E = 210e9 
-nu = 0.3
-mu    = Constant(E / (2.0*(1.0 + nu)))
-lambda_ = Constant(E*nu / ((1.0 + nu)*(1.0 - 2.0*nu)))
+mu = 80.77 #Ambati et al
+lambda_ = 121.15 #Ambati et al
+#E = 210 #210e9 
+#nu = 0.3
+#mu    = Constant(E / (2.0*(1.0 + nu)))
+#lambda_ = Constant(E*nu / ((1.0 + nu)*(1.0 - 2.0*nu)))
 penalty = float(2*mu)
-Gc = 2.7e3 #2.7e3
+Gc = 2.7e-3 #2.7e3
 #k = 1.e-4 #loading speed
-t_init = 1e-6 #2.7e-5 #check that
+t_init = 9e-3 #1e-6 #2.7e-5 #check that
 
-#sample dimensions
-Ll, l0, H = 1e-3, 0.5e-3, 1e-3
+##sample dimensions
+#Ll, l0, H = 1e-3, 0.5e-3, 1e-3
 
 #mesh
 #size_ref = 100 #80 #40 #20 #10
@@ -30,12 +29,16 @@ Ll, l0, H = 1e-3, 0.5e-3, 1e-3
 #folder = 'structured'
 folder = 'unstructured'
 mesh = Mesh()
+size_ref = 4 #4 for 'test' #5 for 'test_2'
+with XDMFFile("mesh/test.xdmf") as infile:
+    infile.read(mesh)
+Ll, l0, H = 1, 0.5, 1 #for this sample...
 #size_ref = 2
 #with XDMFFile("mesh/fine.xdmf") as infile:
 #    infile.read(mesh)
-size_ref = 1
-with XDMFFile("mesh/coarse.xdmf") as infile:
-    infile.read(mesh)
+#size_ref = 1
+#with XDMFFile("mesh/coarse.xdmf") as infile:
+#    infile.read(mesh)
 #size_ref = 3
 #with XDMFFile("mesh/very_fine.xdmf") as infile:
 #    infile.read(mesh)
@@ -171,7 +174,7 @@ A_not_D,B = problem.schur_complement(A)
 
 #definition of time-stepping parameters
 chi = 1
-dt = 1e-7 #1e-7 #ref
+dt = 1e-4 #1e-7 #ref
 print('dt: %.5e' % dt)
 T = 1e1 #0.01e-3 #max in theory
 
@@ -192,9 +195,6 @@ while u_D.t < T:
         #inverting system
         count += 1
         print('COUNT: %i' % count)
-
-        #old solve
-        #u_reduced = spsolve(A_not_D, L_not_D)
 
         #test new solve
         u_reduced = Solve(to_PETScMat(A_not_D), to_PETScVec(L_not_D))
@@ -227,8 +227,10 @@ while u_D.t < T:
             
 
             #jeremy
-            v = problem.trace_matrix.T * interpolate(Constant((1,0)), U_CR).vector().get_local()
+            v = Expression(('(x[1]+0.5*L)/L', '0'), L=Ll, degree=1)
+            v = problem.trace_matrix.T * interpolate(v, U_CR).vector().get_local()
             load_test = np.dot(A*u - problem.trace_matrix.T * L[:problem.initial_nb_dof_CR], v)
+            #load_test = np.dot(A*u, v)
             ld.write('%.5e %.5e %.5e\n' % (u_D.t, assemble(load), load_test))
 
         cracking_facets = set()